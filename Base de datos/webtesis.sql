0b07ec18b9c78c2f806fcd632bb707234b4987cf
LuisFerOv25 <ingluisfer25@udenar.edu.co>
2022-09-08 15:00:12 -0500

    Fix Bugs con la BD

--- Base de datos/webtesis.sql
+++ Base de datos/webtesis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-09-2022 a las 05:38:01
+-- Tiempo de generación: 08-09-2022 a las 21:58:15
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,56 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `trabajogrado`
+-- Estructura de tabla para la tabla `persona`
 --
 
-CREATE TABLE `trabajogrado` (
-  `idTrabajoGrado` int(11) NOT NULL,
+CREATE TABLE `persona` (
+  `idpersona` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  `fechacar` varchar(45) NOT NULL,
-  `rutaArchivo` varchar(200) NOT NULL,
-  `codigoestudiante` int(11) NOT NULL,
-  `docasig` int(11) DEFAULT NULL,
-  `numedit` int(11) DEFAULT NULL
+  `apellido` varchar(45) NOT NULL,
+  `tipoide` varchar(45) NOT NULL,
+  `numdoc` char(10) NOT NULL,
+  `celular` char(12) NOT NULL,
+  `correo` varchar(90) NOT NULL,
+  `pass` varchar(45) NOT NULL,
+  `sexo` char(2) NOT NULL,
+  `idrol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `trabajogrado`
+-- Volcado de datos para la tabla `persona`
 --
 
-INSERT INTO `trabajogrado` (`idTrabajoGrado`, `nombre`, `fechacar`, `rutaArchivo`, `codigoestudiante`, `docasig`, `numedit`) VALUES
-(1, 'Sistema de información para gestion de la tie', '2022-04-11', '../archivoTesis/sprint1.pdf', 21876543, 2, NULL);
+INSERT INTO `persona` (`idpersona`, `nombre`, `apellido`, `tipoide`, `numdoc`, `celular`, `correo`, `pass`, `sexo`, `idrol`) VALUES
+(1, 'luz Idalia', 'Sanchez', 'CC', '1098765434', '3217654321', 'luzidalia123@hotmail.com', '1234', 'F', 1),
+(2, 'Favio Fausto', 'Davila', 'CC', '1987654345', '3214567898', 'FavioFausto34@udenar.edu.co', '1234', 'M', 2),
+(3, 'Camila Ordo', 'Goyes', 'CC', '1098765345', '3215678654', 'Camilaordo45@udenar.edu.co', '1234', 'F', 3);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `trabajogrado`
+-- Indices de la tabla `persona`
 --
-ALTER TABLE `trabajogrado`
-  ADD PRIMARY KEY (`idTrabajoGrado`),
-  ADD KEY `codigoestudiante` (`codigoestudiante`);
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idpersona`),
+  ADD KEY `idrol` (`idrol`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `trabajogrado`
+-- AUTO_INCREMENT de la tabla `persona`
 --
-ALTER TABLE `trabajogrado`
-  MODIFY `idTrabajoGrado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `persona`
+  MODIFY `idpersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `trabajogrado`
+-- Filtros para la tabla `persona`
 --
-ALTER TABLE `trabajogrado`
-  ADD CONSTRAINT `trabajogrado_ibfk_1` FOREIGN KEY (`codigoestudiante`) REFERENCES `estudiante` (`codigoestudiante`) ON UPDATE CASCADE;
+ALTER TABLE `persona`
+  ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idrol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
